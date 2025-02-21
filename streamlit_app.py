cc9943c0104601f1098e3ebe7bff7635b5f61d46
UnarmedMoth <s.limbu@bellevuecollege.edu>
2025-02-21 02:37:29 +0000

    Updating title.v2

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new Streamlit app")
+st.title("🎈 Testing my first ever app build. SEC460 class. ")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
